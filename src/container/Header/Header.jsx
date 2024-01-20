1a0eaebecebbd9dc1fd743e782e9492489dce436
tedstar25 <you@example.com>
2024-01-20 10:17:58 +0100

    implemented header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import {images} from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
+
+
+
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id="home">
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1' >The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Ein gastronomischer Texter ist ein Fachmann oder Werbetexter, der für das Schreiben von Fachtexten im Gastgewerbe qualifiziert ist . Mit ihren Dienstleistungen können sie das monatliche/jährliche monetäre Einkommen des Lokals optimieren, indem sie Texte erstellen , die zum Handeln seitens des Kunden motivieren.</p>
+      <button type="button" className='custom__button' >Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+    </div>
   </div>
 );
 
