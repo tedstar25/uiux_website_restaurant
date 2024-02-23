99fa7d08688c931bd2770d30900cf5b211b8e1fe
tedstar25 <you@example.com>
2024-02-23 14:39:37 +0100

    config now working on vps without proxy

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import {Loading, Error404} from './pages'
 
 import { Routes ,Route} from 'react-router-dom';
 
+import dotenv from 'dotenv';
+
+
+
 //https://www.youtube.com/watch?v=4oV65GVVits
 
 const Home = lazy(() => import('./pages/home.jsx'));
